9e87e80e0f612f638c574550a2286d4ad2e6e26d
Devin Polichetti <devinpolichetti@gmail.com>
2023-02-21 15:17:03 -0500

    fetching for the 5 day forcast

--- assets/script.js
+++ assets/script.js
@@ -14,3 +14,31 @@ async function fetchApiGeoData(city) {
 
   return apiObj;
 }
+// fetch current weather data based on latitude and longitude passed
+async function fetchApiWeatherData(lat, lon) {
+  let apiKey = "60d38292445dbe1a20b97c43b4fe3bd2";
+  let apiObj = {};
+
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+  )
+    .then((response) => response.json())
+    .then((response) => (apiObj = response))
+    .catch((err) => console.error(err));
+
+  return apiObj;
+}
+// fetch 5 day forecast weather data based on latitude and longitude passed
+async function fetchApiForecastData(lat, lon) {
+  let apiKey = "60d38292445dbe1a20b97c43b4fe3bd2";
+  let apiObj = {};
+
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+  )
+    .then((response) => response.json())
+    .then((response) => (apiObj = response))
+    .catch((err) => console.error(err));
+
+  return apiObj;
+}
